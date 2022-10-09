355ae47f30624e12d158d8a7f57e3374e8f1164b
DifexRV <roOmkin@gmail.com>
2022-10-09 22:10:50 +0700

    Ветка для ДЗ 3/3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    System.out.println("Задача 1");
+        int today = 2022;
+        int toTheCurrent200 = today - 200;
+        int afterTheCurrent100 = today + 100;
+        for (int i = 0; i <= 3000; i = i + 79) {
+            if (toTheCurrent200 <= i && i <= afterTheCurrent100)
+                System.out.println(i);
+            }
+
+    System.out.println("Задача 2");
+        int x = 2;
+        for(int i = 1; i < 11; i++) {
+
+            System.out.println(x + "*" + i + "=" + x * i);
+
+        }
+
+
+
 
     }
 }
